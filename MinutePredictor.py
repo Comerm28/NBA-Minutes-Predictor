8d6f05ee4c1bf67762c4912e7cd4d7d7904fe2c6
Comerm28 <100499004+Comerm28@users.noreply.github.com>
2024-05-20 12:24:33 -0400

    better files that work with a demonstration

--- MinutePredictor.py
+++ MinutePredictor.py
@@ -5,25 +5,39 @@ from sklearn.metrics import mean_absolute_error
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import joblib
 
 pd.set_option('display.max_columns', None)
 
-df = pd.read_csv('/Users/mitch/Desktop/VSCode/NBAStatPredictor/DataSet.csv')
-# print(df)
+def createModel():
+    df = pd.read_csv('/Users/mitch/Desktop/VSCode/NBAStatPredictor/DataSet.csv')
+    # print(df)
 
-x = df.drop(columns = ['id', 'full_name', 'first_name', 'last_name', 'is_active', 'minAve'])
-# print(x)
+    x = df.drop(columns = ['id', 'full_name', 'first_name', 'last_name', 'is_active', 'minAve'])
+    # print(x)
 
-y = df['minAve']
-# print(y)
+    y = df['minAve']
+    # print(y)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2)
 
-reg = LinearRegression()
+    reg = LinearRegression()
 
-reg.fit(x_train, y_train)
+    reg.fit(x_train, y_train)
 
-y_pred_test = reg.predict(x_test)
+    y_pred_test = reg.predict(x_test)
 
-print(r2_score(y_test, y_pred_test))
-print(mean_absolute_error(y_test, y_pred_test))
+    print('R^2:', r2_score(y_test, y_pred_test))
+    print('Mean absolute error:', mean_absolute_error(y_test, y_pred_test))
+
+    joblib.dump(reg, 'currentmodel.pkl')
+
+# createModel()
+
+reg = joblib.load('currentmodel.pkl')
+
+ex = pd.read_csv('/Users/mitch/Desktop/VSCode/NBAStatPredictor/DataDemonstration.csv')
+xDemonstrate = ex.drop(columns = ['minAve'])
+
+print('Jaden McDaniels, Naz Reid, and Nickeil Alexander-Walker\'s MPG:', reg.predict(xDemonstrate))
+print('Actual: 27.588028169014084, 19.23926380368098, 19.62541806020067')
